d99ce9351a1be26d59affd4bd04057c759f10f5f
emmasimp <emmasimp@users.noreply.github.com>
2021-10-12 16:16:16 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 09:54:04 2021
+
+@author: emma
+
+Streamlit workshope Septembrse
+"""
+
+import streamlit as st
+
+st.title('My first streamlit app')
+
+a = 1.23
+
+st.write(a)
+
+
+st.markdown('Text can be **bold** and _italic_')
